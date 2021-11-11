cb8ffd2e4a6b808e695229b515bc29fb95e3d91f
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-11-11 23:52:08 +0000

    Começo do exercício 3 de java

---
+++ Aluno.java
@@ -0,0 +1,67 @@
+public class Aluno{
+    private String nome;
+    private String matricula;
+    private String curso;
+    private String materia1;
+    private String materia2;
+    private String materia3;
+    private double notaMateria1;
+    private double notaMateria2;
+    private double notaMateria3;
+
+    public double getNotaMateria1() {
+        return notaMateria1;
+    }
+    public void setNotaMateria1(double notaMateria1) {
+        this.notaMateria1 = notaMateria1;
+    }
+    public double getNotaMateria2() {
+        return notaMateria2;
+    }
+    public void setNotaMateria2(double notaMateria2) {
+        this.notaMateria2 = notaMateria2;
+    }
+    public double getNotaMateria3() {
+        return notaMateria3;
+    }
+    public void setNotaMateria3(double notaMateria3) {
+        this.notaMateria3 = notaMateria3;
+    }
+    public String getMateria1() {
+        return materia1;
+    }
+    public void setMateria1(String materia1) {
+        this.materia1 = materia1;
+    }
+    public String getMateria2() {
+        return materia2;
+    }
+    public void setMateria2(String materia2) {
+        this.materia2 = materia2;
+    }
+    public String getMateria3() {
+        return materia3;
+    }
+    public void setMateria3(String materia3) {
+        this.materia3 = materia3;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    } 
+    
+}
